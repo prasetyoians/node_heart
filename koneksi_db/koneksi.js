8b3d76f7404ffc29348049d772be3196bc657e80
prasetyoians <108276050+prasetyoians@users.noreply.github.com>
2023-11-13 20:43:38 +0700

    Update koneksi.js

--- koneksi_db/koneksi.js
+++ koneksi_db/koneksi.js
@@ -13,7 +13,7 @@
 const { Pool } = require('pg');
 
 module.exports = new Pool({
-  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require", 
 });
 
 // client.connect();
