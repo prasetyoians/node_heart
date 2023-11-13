c5ef653d1e86389f97a183bc85cce56a6b6b276f
prasetyoians <108276050+prasetyoians@users.noreply.github.com>
2023-11-13 23:48:21 +0700

    Update app.js

--- app.js
+++ app.js
@@ -83,8 +83,8 @@ app.get('/tes', (req, res) => {
 		 	return res.end();
 		 }else{
 		    	res.writeHead(200,{'Content-type':'text/html'});
-		   data = replaceIncludes(data, path.join(__dirname, 'views'));
-
+		   // data = replaceIncludes(data, path.join(__dirname, 'views'));
+			console.log(viewPath);
 		 	return res.end();	
 		 }
 	});
