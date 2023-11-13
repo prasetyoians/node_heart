e438e57c116471366f714affd6f417b405f23689
prasetyoians <108276050+prasetyoians@users.noreply.github.com>
2023-11-13 15:23:16 +0700

    perubahan koneksi

--- koneksi_db/koneksi.js
+++ koneksi_db/koneksi.js
@@ -13,11 +13,7 @@
 const { Pool } = require('pg');
 
 module.exports = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'alkes',
-  password: 'ianprast1234567',
-  port: 5432 // Default port for PostgreSQL
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
 // client.connect();
